8e918f5930de1a51fa5b615fe7e6a01252e79b61
Cynthia <cynthiacruz1007@gmail.com>
2024-01-25 16:30:47 +0100

    prueba

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { IUser, useStateContext } from "../../state/auth.state";
 import "./LoginForm.css";
-// Utilizando Bcrypt para el hash de contraseñas con el objetivo de mejorar la seguridad.
+// Utilizando Bcrypt para el hash de contraseñas con el objetivo de mejorar la seguridad..
 import bcrypt from "bcryptjs";
 
 const LoginForm = () => {
